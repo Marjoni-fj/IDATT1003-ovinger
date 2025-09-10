a24deeec11cdf39ec283fe927da64316c705ee4e
Fredrik Marjoni <marjonifredrik@gmail.com>
2025-09-10 13:02:36 +0200

    Feat[]: Add constructor printMatrise to print out the matrix

--- oving6/src/ooving6_oppg3/Matriser.java
+++ oving6/src/ooving6_oppg3/Matriser.java
@@ -1,17 +1,27 @@
 package ooving6_oppg3;
 
 public class Matriser {
+    int rows;
+    int cols;
+    int[][] data;
 
-
-
-
-    //public double adderMatriser(double matrise2){
-        //return matrise1 + matrise2;
-    //}
-
-
-
-
-
+   
+    // Konstruktør som tar inn et ferdig 2D-array
+    public Matriser(int[][] values) {
+        this.rows = values.length;
+        this.cols = values[0].length;
+        this.data = values;
+    }
     
+
+   public void printMatrise() {
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(data[i][j] + " ");
+            }  
+            System.out.println();
+        }
+    }
+
+
 }
--- oving6/src/ooving6_oppg3/oppg3.java
+++ oving6/src/ooving6_oppg3/oppg3.java
@@ -1,5 +1,16 @@
 package ooving6_oppg3;
 
 public class oppg3 {
+    public static void main(String[] args){
+
+        Matriser m2 = new Matriser(new int[][] {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        });
+
+        m2.printMatrise();
+
+    }
     
 }
