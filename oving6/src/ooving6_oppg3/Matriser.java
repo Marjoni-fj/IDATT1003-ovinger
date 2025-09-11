bf398152317783559f2af0cda7443976575b27c9
Fredrik Marjoni <marjonifredrik@gmail.com>
2025-09-11 11:30:06 +0200

    Feat[]: Fullført ooving6_oppg3 ved å fikse matriseproduktet

--- oving6/src/ooving6_oppg3/Matriser.java
+++ oving6/src/ooving6_oppg3/Matriser.java
@@ -1,19 +1,28 @@
 package ooving6_oppg3;
 
 public class Matriser {
-    int rows;
-    int cols;
-    int[][] data;
+    private final int rows;
+    private final int cols;
+    private final int[][] data;
 
    
     // Konstruktør som tar inn et ferdig 2D-array
     public Matriser(int[][] values) {
         this.rows = values.length;
         this.cols = values[0].length;
-        this.data = values;
+        this.data = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            if (values[i].length != cols) {
+                throw new IllegalArgumentException("Alle rader må ha samme lengde");
+            }
+            for (int j = 0; j < cols; j++) {
+                this.data[i][j] = values[i][j];
+            }
+        }
     }
     
-
+    
+    //Printer ut matrisen
    public void printMatrise() {
         for (int i = 0; i < rows; i++) {
             for (int j = 0; j < cols; j++) {
@@ -22,6 +31,68 @@ public class Matriser {
             System.out.println();
         }
     }
+
+    @Override
+    public String toString() {
+    StringBuilder sb = new StringBuilder();
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            sb.append(data[i][j]).append(" ");
+        }
+        sb.append("\n");
+    }
+    return sb.toString();
+}
+
+    // Legger sammen to matriser (returnerer null hvis dimensjonene ikke passer)
+    public Matriser addMatriser(Matriser A) {
+        if (A == null || A.rows != this.rows || A.cols != this.cols) {
+            return null; // operasjonen er umulig
+        }
+
+        int[][] result = new int[rows][cols];
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                result[i][j] = this.data[i][j] + A.data[i][j];
+            }
+        }
+        return new Matriser(result);
+    }
+    //Transponere Matriser
+    public Matriser transponer(){                                                         
+        int[][] result = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                result[j][i] = this.data[i][j];
+            }
+        }
+        return new Matriser(result);
+
+    }
+
+    public Matriser matriseProduktet(Matriser A){
+        if (A == null || this.cols != A.rows){
+            return null;
+        }
+        int[][] result = new int[this.rows][A.cols];
+
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                int sum = 0;
+                for (int k = 0; k < cols; k++){
+                    sum += this.data[i][k] * A.data[k][j];
+                } 
+                result[i][j] = sum;              
+            }
+        }
+        return new Matriser(result);
+
+
+        
+    }
+
+    
 
 
 }
--- oving6/src/ooving6_oppg3/oppg3.java
+++ oving6/src/ooving6_oppg3/oppg3.java
@@ -1,5 +1,6 @@
 package ooving6_oppg3;
 
+
 public class oppg3 {
     public static void main(String[] args){
 
@@ -8,9 +9,32 @@ public class oppg3 {
             {4, 5, 6},
             {7, 8, 9}
         });
+        Matriser m3 = new Matriser(new int[][] {
+            {2,4,6},
+            {8,10,12},
+            {14,16,18}
+        });
+        
+        
+        //Adding Matrix m2 and m3 together
+        Matriser m4 = m2.addMatriser(m3);
+        System.out.println("M2 + M3:");
+        System.out.println(m2 + " + \n" + m3 + " = \n" + m4);
 
-        m2.printMatrise();
+        //Transponizing m2
+        System.out.println("Transponert M2:");
+        Matriser m5 = m2.transponer();
+        System.out.println(m2 + "^T = \n" + m5);
+
+        //Matrisemultiplikasjon 
+        System.out.println("M2 * M3");
+        Matriser m6 = m2.matriseProduktet(m3);
+        System.out.println(m2 + " * \n" + m3 + " = \n" + m6);
+
 
     }
+
+    
+
     
 }
