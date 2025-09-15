858b520fad98ce259498d6bf45775ddcdd8f74e3
Fredrik Marjoni <marjonifredrik@gmail.com>
2025-09-15 09:26:26 +0200

    feat[]: begynne med ooving6_oppg2 med å skrive deler av klassen og dens konstruktør og metoder

---
+++ oving6/src/ooving6_oppg2/Tekstanalyse.java
@@ -0,0 +1,44 @@
+package ooving6_oppg2;
+
+public class Tekstanalyse {
+    String text;
+    int[] antallTegn = new int[30];
+
+    public Tekstanalyse(String text){
+        this.text = text;
+
+
+    }
+
+    public int antallForskjelligeBokstaver(int antall){
+        return antall;
+
+    }
+
+    public int totaltAntallBokstaver(int total){
+        return total;
+
+    }
+
+    public double prosentIkkeBokstaver(double prosent){
+        return prosent;
+
+    }
+
+    public int antallAv(char bokstav){
+        return bokstav;
+    }
+    /*
+
+    public int mestForekommendeBokstaver(){
+       
+    }
+
+    */
+
+
+
+
+
+
+}
