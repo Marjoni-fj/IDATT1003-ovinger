7d7a7251720c960d892a426bf8853995028e0820
Fredrik Marjoni <marjonifredrik@gmail.com>
2025-09-15 10:04:39 +0200

    feat[] forbedrer og utvider konstruktøren min gjennom en private metode som inkluderer de norske bokstavene

--- oving6/src/ooving6_oppg2/Tekstanalyse.java
+++ oving6/src/ooving6_oppg2/Tekstanalyse.java
@@ -4,11 +4,22 @@ public class Tekstanalyse {
     String text;
     int[] antallTegn = new int[30];
 
-    public Tekstanalyse(String text){
-        this.text = text;
+    private int finnIndeks(char tegn) {
+    tegn = Character.toLowerCase(tegn);
+    if (tegn >= 'a' && tegn <= 'z') return tegn - 'a';
+    if (tegn == 'æ') return 26;
+    if (tegn == 'ø') return 27;
+    if (tegn == 'å') return 28;
+    return 29;
+}
 
-
+    public Tekstanalyse(String text) {
+    this.text = text;
+    for (int i = 0; i < text.length(); i++) {
+    int indeks = finnIndeks(text.charAt(i));
+    antallTegn[indeks]++;
     }
+}
 
     public int antallForskjelligeBokstaver(int antall){
         return antall;
