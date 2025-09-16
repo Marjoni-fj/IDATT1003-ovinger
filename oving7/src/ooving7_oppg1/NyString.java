45b3d1eb9c96c39335da233d2dbe71dfff8f1c2a
Fredrik Marjoni <marjonifredrik@gmail.com>
2025-09-16 09:58:43 +0200

    feat[]:gjøre klar metodene til ooving7 oppg1

---
+++ oving7/src/ooving7_oppg1/NyString.java
@@ -0,0 +1,24 @@
+package ooving7_oppg1;
+
+public class NyString {
+    private final String setning;
+
+
+    public NyString(String setning){
+        this.setning = setning;
+
+    }
+
+    public String getSetning(){
+        return setning;
+    }
+
+    public String forkort(String setning){
+        return setning;
+    }
+
+    public String fjernBokstav(char bokstav){
+        return setning;
+    }
+    
+}
