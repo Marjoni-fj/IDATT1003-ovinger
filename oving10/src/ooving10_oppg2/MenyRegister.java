282113c307665a51e5028e87ce2c7f0a4b965d2a
Fredrik Marjoni <marjonifredrik@gmail.com>
2025-09-30 09:14:29 +0200

    feat[]: begin MenyRegister by giving it attribute list of Rett objects

--- oving10/src/ooving10_oppg2/MenyRegister.java
+++ oving10/src/ooving10_oppg2/MenyRegister.java
@@ -1,5 +1,16 @@
 package ooving10_oppg2;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class MenyRegister {
+    private List<Rett> meny;
+
+    public MenyRegister() {
+        meny = new ArrayList<>();
+    }
     
+    public List<Rett> getMeny() {
+        return meny;
+    }
 }
