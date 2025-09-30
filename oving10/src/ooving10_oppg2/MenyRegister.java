ab1e3ddb6aa4edb5659b19da607be6edc3c9559d
Fredrik Marjoni <marjonifredrik@gmail.com>
2025-09-30 12:00:14 +0200

    feat[]: add get method and methods to find food based on name or type

--- oving10/src/ooving10_oppg2/MenyRegister.java
+++ oving10/src/ooving10_oppg2/MenyRegister.java
@@ -4,13 +4,32 @@ import java.util.List;
 
 
 public class MenyRegister {
-    private List<Rett> meny;
+    private List<Rett> meny; //ArrayList med Rett objekter
 
-    public MenyRegister() {
+    public MenyRegister() { //konstruktør som lager Arraylist
         meny = new ArrayList<>();
     }
     
-    public List<Rett> getMeny() {
+    public List<Rett> getMeny() { //Get metode for å hente menyen
         return meny;
+    }
+    public void finnRettSomHeter(String navn){ //finner en spesifikt rett i menyen basert på navn
+        for (Rett rett : meny) {
+            if (rett.getNavn().equalsIgnoreCase(navn)) {
+                System.out.println(rett);
+            }
+            else{ System.out.println("Vi har desverre ikke" + navn + "på denne menyen");}
+            
+        }
+    }
+
+    public void finnRettAvTypen(String type){ //finner en spesifikt rett i menyen basert på type
+        for (Rett rett : meny){
+            if (rett.getType().equalsIgnoreCase(type)){
+                System.out.println(rett);
+            }
+            else { System.out.println("Vi har desverre ikke slike rett typer på denne menyen");}
+        }
+
     }
 }
