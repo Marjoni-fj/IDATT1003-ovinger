1382e9f7cbcb240b96b3aafd3dd3edab0cb8f7ef
Fredrik Marjoni <marjonifredrik@gmail.com>
2025-09-30 13:54:43 +0200

    Feat[]: add attribute oppskrift and update Rett class

--- oving10/src/ooving10_oppg2/Rett.java
+++ oving10/src/ooving10_oppg2/Rett.java
@@ -4,22 +4,25 @@ public class Rett {
     private final String navn;
     private String type;
     private double pris;
+    private String oppskrift;
 
 
-    public Rett(String navn, String type, double pris){
+    public Rett(String navn, String type, double pris, String oppskrift){
         this.navn = navn;
         this.type = type;
         this.pris = pris;
+        this.oppskrift = oppskrift;
     }
     public String getNavn(){ return navn; }
     public String getType(){ return type; }
     public double getPris(){ return pris; }
+    public String getOppskrift(){ return oppskrift; }
 
     @Override
     public String toString() {
         return String.format(
-            "Navn: %s | Type: %s | Pris: %.2f",
-            navn, type, pris
+            "Navn: %s | Type: %s | Pris: %.2f | Oppskrift: %s",
+            navn, type, pris, oppskrift
         );
 
     
