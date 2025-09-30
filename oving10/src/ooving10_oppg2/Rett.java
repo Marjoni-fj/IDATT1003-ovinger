85b1efe9ab80a9991f1b65a92d07b2097b9692f4
Fredrik Marjoni <marjonifredrik@gmail.com>
2025-09-30 08:26:41 +0200

    feat[]: add attribute final navn to class Rett

--- oving10/src/ooving10_oppg2/Rett.java
+++ oving10/src/ooving10_oppg2/Rett.java
@@ -1,15 +1,17 @@
 package ooving10_oppg2;
 
 public class Rett {
+    private final String navn;
     private String type;
     private double pris;
 
 
-    public Rett(String type, double pris){
+    public Rett(String navn, String type, double pris){
+        this.navn = navn;
         this.type = type;
         this.pris = pris;
     }
-
+    public String getNavn(){ return navn; }
     public String getType(){ return type; }
     public double getPris(){ return pris; }
 
