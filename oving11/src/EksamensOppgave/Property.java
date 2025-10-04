6957dc359a496b7ac1d6fdb859efae94d341e7f9
Fredrik Marjoni <marjonifredrik@gmail.com>
2025-10-04 18:40:36 +0200

    feat[]: change name on propertyname->propertyName. Add set methods to set attributes that seems reasonable to have the ability to change values

--- oving11/src/EksamensOppgave/Property.java
+++ oving11/src/EksamensOppgave/Property.java
@@ -5,17 +5,17 @@ public class Property {
     private String municipalityName;
     private int lotNr;
     private int sectionNr;
-    private String propertyname;
+    private String propertyName;
     private double area;
     private String owner;
 
     public Property(int municipalityNr, String municipalityName, int lotNr, 
-                    int sectionNr,  String propertyname, double area, String owner){
+                    int sectionNr,  String propertyName, double area, String owner){
         this.municipalityNr = municipalityNr;
         this.municipalityName = municipalityName;
         this.lotNr = lotNr;
         this.sectionNr = sectionNr;
-        this.propertyname = propertyname;
+        this.propertyName = propertyName;
         this.area = area;
         this.owner = owner;
 
@@ -25,8 +25,23 @@ public class Property {
     public String getMunicipalityName(){ return municipalityName;}
     public int getLotNr(){ return lotNr;}
     public int getSectionNr(){ return sectionNr;}
-    public String getPropertyName(){ return propertyname;}
+    public String getPropertyName(){ return propertyName;}
     public double getArea(){ return area;}
     public String getOwner(){ return owner;}
+
+
+    /**
+     * Grunnen til at kommunenr, gnr og bnr ikke har settere,
+     * er at disse verdiene entydig identifiserer eiendommen,
+     * og bør ikke kunne endres etter opprettelse.
+     */
+    public void setSectionNr(int sectionNr) { this.sectionNr = sectionNr;}
+
+    public void setArea(double area) {
+        if (area > 0)
+            this.area = area;
+    }
+
+    public void setOwner(String owner) { this.owner = owner;}
     
 }
